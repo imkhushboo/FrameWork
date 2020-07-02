3f846ee50ea45581815bd3159736acb08059fcf7
khushboo-lab <vkhushboo540@gmail.com>
2020-07-02 19:19:23 +0530

    reactcompaonent1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponents';
+//import './App.css';
 
 function App() {
   return (
-   <div className="App">
-     <Navbar dark color="primary">
-       <div className="container">
-         <NavbarBrand href="/">Ristorente Con Fusion
-         </NavbarBrand>
-       </div>
-     </Navbar>
+   <div>
+    <Navbar dark color="primary">
+    <NavbarBrand href="/">Ristorente Con Fusion</NavbarBrand>
+    </Navbar>
+     <Menu />
    </div>
   );
 }
